fdde8bea674b42b12f2984869ea72631b42164ed
Prajeeth777 <prajeethkumar777@gmail.com>
2024-12-14 20:30:34 +0900

    Lesson 32

---
+++ Activity 4.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+
+print("Before swapping first number is ",num1 )
+print("Before swapping second number is ",num2 )
+temp=num1
+num1=num2
+num2=temp
+
+print("After swapping first number is ",num1)
+print("After swapping second number is ",num2)
