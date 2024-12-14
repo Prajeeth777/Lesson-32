295a6895ee70e2e195df5cd245535025cee99d47
Prajeeth777 <prajeethkumar777@gmail.com>
2024-12-14 20:05:48 +0900

    Lesson 32

---
+++ Activity 2.py
@@ -0,0 +1,14 @@
+num1=10
+num2=20
+
+print("Sum of ",num1," and ",num2," is ",(num1+num2))
+print("Difference of ",num1," and ",num2," is ",(num1-num2))
+print("Product of ",num1," and ",num2," is ",(num1*num2))
+print("Div of ",num1," and ",num2," is ",(num2/num1))
+print("Floor Div of ",num1," and ",num2," is ",(num2//num1))
+print("Remainder of ",num1," and ",num2," is ",(num2%num2))
+print("2 to the power of 2 ",(2**2))
+
+print("is ",num1,"equal to ",num2, num1==num2)
+print("is ",num1,"greater to ",num2, num1>num2)
+print("is ",num1,"smaller to ",num2, num1<num2)
