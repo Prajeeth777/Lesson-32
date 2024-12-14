e0e267c935e62343a4f72015b05a51b510c738fa
Prajeeth777 <prajeethkumar777@gmail.com>
2024-12-14 20:22:36 +0900

    Lesson 32

---
+++ Activity 3.py
@@ -0,0 +1,13 @@
+firstname="Prajeeth"
+lastname="Yaswanth Kumar"
+fullname=firstname+lastname
+
+print("Full Name is ",fullname)
+
+print("Firstname multiplied by 3 is ",firstname*3)
+
+word="Hello World"
+print("Length of ",word," is ",len(word))
+print("First Letter of ",word," is ",word[0])
+print("Last Letter of ",word," is ",word[10])
+print("Sliced word is ",word[1:7])
